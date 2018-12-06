a0e56c40eaac68140f35dafc3abc56bdb179785a
moqinggen <mo1056610188@163.com>
2018-12-06 16:52:07 +0800

    角色权限

--- sql/database.sql
+++ sql/database.sql
@@ -8,29 +8,59 @@ GRANT ALL PRIVILEGES ON njit_report.* TO 'njittss'@'%' IDENTIFIED BY 'njittss!@#
 GRANT ALL PRIVILEGES ON njit_experiment.* TO 'njittss'@'%' IDENTIFIED BY 'njittss!@#$5678' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
--- CREATE TABLE `njit_admin` (
---   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
---   PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
---
--- CREATE TABLE `njit_privilege` (
---   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
---   PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
---
--- CREATE TABLE `njit_role` (
---   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
---   PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
---
--- CREATE TABLE `njit_role_privilege` (
---   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
---   PRIMARY KEY (`id`)
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- 角色权限表
+CREATE TABLE `njit_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `code` varchar(32) DEFAULT '' COMMENT '角色code',
+  `name` varchar(50) DEFAULT '' COMMENT '角色名',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `del_flag` TINYINT DEFAULT 0 COMMENT '删除标志 0正常 1删除',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
+INSERT INTO `njit_role` VALUES ('1', 'ADMIN', '管理员', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role` VALUES ('2', 'SYY', '实验员', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role` VALUES ('3', 'TEACHER', '教师', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role` VALUES ('4', 'STUDENT', '学生', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
 
+CREATE TABLE `njit_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `permission` varchar(32) DEFAULT '' COMMENT '权限标识',
+  `name` varchar(50) DEFAULT '' COMMENT '名称',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `del_flag` TINYINT DEFAULT 0 COMMENT '删除标志 0正常 1删除',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `permission` (`permission`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='权限表';
+
+INSERT INTO `njit_permission` VALUES ('1', 'back:permission:save', '保存权限标识', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_permission` VALUES ('2', 'back:permission:update', '修改权限标识', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_permission` VALUES ('3', 'back:permission:delete', '删除权限标识', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_permission` VALUES ('4', 'back:permission:query', '查询权限标识', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_permission` VALUES ('5', 'back:role:save', '添加角色', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_permission` VALUES ('6', 'back:role:update', '修改角色', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_permission` VALUES ('7', 'back:role:delete', '删除角色', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_permission` VALUES ('8', 'back:role:permission:set', '给角色分配权限', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+
+CREATE TABLE `njit_role_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `role_id` bigint(20) DEFAULT -1 COMMENT '角色id',
+  `permission_id` int(11) DEFAULT -1 COMMENT '权限id',
+  `create_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `del_flag` TINYINT DEFAULT 0 COMMENT '删除标志 0正常 1删除',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色权限关系表';
+INSERT INTO `njit_role_permission` VALUES ('1', '1', '1', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role_permission` VALUES ('2', '1', '2', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role_permission` VALUES ('3', '1', '3', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role_permission` VALUES ('4', '1', '4', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role_permission` VALUES ('5', '1', '5', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role_permission` VALUES ('6', '1', '6', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role_permission` VALUES ('7', '1', '7', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
+INSERT INTO `njit_role_permission` VALUES ('8', '1', '8', '2018-12-06 12:00:00', '2018-12-06 12:00:00', '0');
 
 CREATE TABLE `njit_student` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
